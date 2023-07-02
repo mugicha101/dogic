234ebb09a220ef538f57da5f4c2e3a25bd8be7c8
Alexander Yoshida <alexander.yoshida@gmail.com>
2023-07-02 17:18:07 -0400

    initial commit

---
+++ app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	boxColor := color.RGBA{255, 0, 255, 255}
+	for x := 100; x < 200; x++ {
+		for y := 100; y < 200; y++ {
+			screen.Set(x, y, boxColor)
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
